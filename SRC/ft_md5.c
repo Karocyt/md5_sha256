0d126c31ae4ee76d1795d32c433f1137a4bcadce
Kevin Azoulay <kevazoul@z2r5p5.le-101.fr>
2019-11-14 14:43:48 +0100

    commit

--- SRC/ft_md5.c
+++ SRC/ft_md5.c
@@ -13,21 +13,30 @@
 
 #include "ft_ssl.h"
 
-static size_t pad_mod512_minus64(unsigned char **input, size_t size)
+static size_t pad_mod512(unsigned char **input, size_t size)
 {
 	int 			new_size;
-	unsigned char	*res;
+	char 			*tmp;
 
-	new_size = (size / 512 + 1) * 512 - 64; // %512 should be 448
-	if (!(res = ft_memalloc(new_size)))
+	new_size = (size / 512 + 1) * 512; // %512 should be 448
+	if (new_size - 64 <= size)
+		new_size += 512;
+	if (!(tmp = ft_memalloc(new_size)))
 		return (0);
-
+	ft_memcpy(tmp, *input, size);
+	tmp[size] = 1;
+	ft_memcpy(tmp + new_size - 64, (unsigned char *)&(uint64_t)size, 8); 
+	free(*input);
+	*input = tmp;
 	return (new_size);
 }
 
 char *ft_md5(unsigned char *input, size_t size)
 {
-	(void)size;
-	(void)input;
+	size = pad_mod512(&input, size);
+	
+
+
+
 	return (ft_strdup("OK\n\n"));
 }
