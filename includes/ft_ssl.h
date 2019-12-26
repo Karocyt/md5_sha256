f68278cee455bf34c041479a4c82c1fec816ebbf
Karocyt/kevazoul <azoulay.kevin+robot@gmail.com>
2019-12-26 20:03:01 +0100

    added md5 constants

--- includes/ft_ssl.h
+++ includes/ft_ssl.h
@@ -24,13 +24,13 @@ typedef enum e_hash_functions {
 	NB_ALGOS = 2,
 	MD5 = 0,
 	SHA256
-}				t_hash;
+}						t_hash;
 
 typedef struct s_item {
 	void			*content;
 	size_t 			size;
 	struct s_item	*next;
-}				t_item;
+}						t_item;
 
 typedef struct s_ssl {
 	uint8_t			p;
@@ -39,22 +39,29 @@ typedef struct s_ssl {
 	uint8_t			s;
 	t_hash			h;
 	t_item			*items;
-}				t_params;
+}						t_params;
 
 typedef union u_md5_words {
 	uint64_t		*uint64;
 	uint32_t		*uint32;
 	unsigned char	*uchar;
-}				t_md5_words;
+}						t_md5_words;
 
-void *g_funcs[NB_ALGOS];
+const uint32_t	g_add[64];
+const uint32_t  g_shift[64];
+const uint32_t	g_init_a;
+const uint32_t	g_init_b;
+const uint32_t	g_init_c;
+const uint32_t	g_init_d;
+
+void 			*g_funcs[NB_ALGOS];
 
 int 			ssl_process(t_params *params);
 int 			ssl_read_params(int ac, char **av, t_params *params);
 char 			*ssl_md5(unsigned char *input, size_t size);
 char 			*ssl_sha256(unsigned char *input, size_t size);
 void    		ssl_clear_items(t_item **lst);
-int        		ssl_add_item_from_str(t_item **lst, char *str);
+int    			ssl_add_item_from_str(t_item **lst, char *str);
 int        		ssl_add_item_from_fd(t_item **lst, int fd);
 size_t 			md5_pad(t_md5_words **words, size_t size);
 
