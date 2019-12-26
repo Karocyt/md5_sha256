1c050318d21b537d1ba3b6923deff1cfd5c65e76
Karocyt/kevazoul <azoulay.kevin+robot@gmail.com>
2019-12-26 20:19:26 +0100

    printfs commented in md5 padding

--- SRC/md5_padding.c
+++ SRC/md5_padding.c
@@ -21,7 +21,7 @@ static size_t md5_pad512_newsize(size_t size)
     new_size = (size / 64 + 1) * 64; // %64 should be 56, but we'll need to append a size_t so should equals 0
     if (new_size - 9 <= size) // 8 for size_t + 1 for required minimal padding
         new_size += 64;
-    ft_printf("size bits: %zu, size %%512bits: %zu\nnew_size bits: %zu, new_size %%512bits: %zu\n", size*8, size*8 % 512, new_size*8, new_size*8 % 512);
+    //ft_printf("size bits: %zu, size %%512bits: %zu\nnew_size bits: %zu, new_size %%512bits: %zu\n", size*8, size*8 % 512, new_size*8, new_size*8 % 512);
     return (new_size);
 
 }
@@ -37,12 +37,12 @@ size_t md5_pad(t_md5_words **words, size_t size)
         return (0);
     if (new_size < size || !(tmp->uchar = ft_memalloc(new_size)))
         return (0);
-    ft_printf("CRAZY SIZE: %zu\n", size);
-    ft_printf("old_size = %zu, new_size = %zu, tmp = %p, size/8 = %zu, offset = %zu\n", size, new_size, &tmp, size / 8, size / 8 * 8);
+    //ft_printf("CRAZY SIZE: %zu\n", size);
+    //ft_printf("old_size = %zu, new_size = %zu, tmp = %p, size/8 = %zu, offset = %zu\n", size, new_size, &tmp, size / 8, size / 8 * 8);
     ft_memcpy(tmp->uchar, *words, size); // seems to go to far and override size ?!
     tmp->uchar[size] = LEADING_ONE; // append bit 1 right after
     ft_memcpy(tmp->uchar + new_size - sizeof(size_t), &size, sizeof(size_t)); // write size at end
-    ft_printf("written size is %lld when size was %lld\n", *((uint64_t *)(tmp->uchar + new_size - 8)), size);
+    //ft_printf("written size is %lld when size was %lld\n", *((uint64_t *)(tmp->uchar + new_size - 8)), size);
 
     free(*words);
     *words = tmp;
