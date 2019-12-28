2079c07f6736d83fdb4660ea1962484421927f40
Kevin Azoulay <kevazoul@z4r8p1.le-101.fr>
2019-12-28 08:25:13 +0100

    md5 seems done

--- Makefile
+++ Makefile
@@ -28,7 +28,6 @@ LFTDIR = libft
 LFT = libft.a
 INCLUDES = -I$(LFTDIR)/includes -Iincludes
 SRC_NAME =	md5_main.c \
-			md5_padding.c \
 			md5_rounds.c \
 			md5_transforms.c \
 			ssl_process.c \
--- SRC/md5_main.c
+++ SRC/md5_main.c
@@ -21,23 +21,28 @@
     https://www.ietf.org/rfc/rfc1321.txt
 */
 
-const uint32_t  g_add[64] = {0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
-                            0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
-                            0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
-                            0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
-                            0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
-                            0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
-                            0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
-                            0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
-                            0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
-                            0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
-                            0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
-                            0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
-                            0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
-                            0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
-                            0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
-                            0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391};
 const t_reg  g_init_reg = {0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476};
+
+size_t md5_pad(t_md5_words **words, size_t size)
+{
+    size_t              new_size;
+    t_md5_words         *tmp;
+
+    if (!(tmp = malloc(sizeof(t_md5_words))))
+        return (0);
+    new_size = size + 1 + 8;
+    while (new_size % 64)
+        new_size++;
+    if (new_size < size || !(tmp->uchar = ft_memalloc(new_size)))
+        return (0);
+    ft_memcpy(tmp->uchar, *words, size);
+    tmp->uchar[size] = LEADING_ONE;
+    
+    tmp->uint64[new_size / 8 - 1] = size * 8;
+    free(*words);
+    *words = tmp;
+    return (new_size);
+}
 
 char    *md5_digest(t_reg r)
 {
@@ -71,8 +76,7 @@ char    *ssl_md5(unsigned char *input, size_t size)
     int i;
     t_reg  main_reg;
 
-
-    t_reg *original = (t_reg *)md5_original(input, size);
+    //t_reg *original = (t_reg *)md5_original(input, size);
     words = (t_md5_words *)input;
 	if (!(size = md5_pad(&words, size)) )
         return (NULL);
@@ -90,7 +94,7 @@ char    *ssl_md5(unsigned char *input, size_t size)
     }
 
     
-    if (!ft_memcpy(original, &main_reg, 16))
-        ft_printf("SUCCESS");
+    //if (!ft_memcpy(original, &main_reg, 16))
+    //    ft_printf("SUCCESS");
     return (md5_digest(main_reg));
 }
